f44f06977156444cc9a9a8e197a3b7de7b7b7684
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2020-03-05 20:01:06 -0300

    Config to use google maps on IOS

--- ios/MapProject/AppDelegate.m
+++ ios/MapProject/AppDelegate.m
@@ -10,6 +10,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 @implementation AppDelegate
 
@@ -18,7 +19,10 @@
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"MapProject"
+
                                             initialProperties:nil];
+NSString* mapsApiKey = [[NSProcessInfo processInfo] environment[@"GOOGLE_KEY"];
+[GMSServices provideAPIKey:mapsApiKey];
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
